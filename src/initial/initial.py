e1f9fff3bd6e7fe371d590830e893c48f12714f6
OU Jiaming <qq15815732077@gmail.com>
2023-08-07 14:39:19 +0800

    update new

--- src/initial/initial.py
+++ src/initial/initial.py
@@ -9,14 +9,48 @@ def load_text(input_file):
     return text
 
 def clean_text(text):
-    """Lowercase and remove punctuation from a string."""
+    """Lowercase and remove punctuation from a string.
+
+    Parameters
+    ----------
+    text : str
+        Text to clean.
+
+    Returns
+    -------
+    str
+        Cleaned text.
+
+    Examples
+    --------
+    >>> clean_text("Early optimization is the root of all evil!")
+    'early optimization is the root of all evil'
+    """
     text = text.lower()
     for p in punctuation:
         text = text.replace(p, "")
     return text
 
 def count_words(input_file):
-    """Count unique words in a string."""
+    """Count words in a text file.
+
+    Words are made lowercase and punctuation is removed
+    before counting.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to text file.
+
+    Returns
+    -------
+    collections.Counter
+        dict-like object where keys are words and values are counts.
+
+    Examples
+    --------
+    >>> count_words("text.txt")
+    """
     text = load_text(input_file)
     text = clean_text(text)
     words = text.split()
