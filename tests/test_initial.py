7db37fcadc7004ce1e884d4f77575a6ee2549da5
OU Jiaming <qq15815732077@gmail.com>
2023-08-07 10:24:28 +0800

    test: add unit test for count_words

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,6 +1,15 @@
 from initial import initial
-
+from collections import Counter
 from initial.initial import count_words
 
 count_words("zen.txt")
 print(count_words("zen.txt"))
+
+def test_count_words():
+    """Test word counting from a file."""
+    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1,
+                        'the': 1, 'same': 1, 'thing': 1,
+                        'over': 2, 'and': 2, 'expecting': 1,
+                        'different': 1, 'results': 1})
+    actual = count_words("tests/einstein.txt")
+    assert actual == expected, "Einstein quote counted incorrectly!"
