50f4320a1739b3b51c2ea8b884be2e95ad17f0e1
OU Jiaming <qq15815732077@gmail.com>
2023-08-08 17:44:37 +0800

    progress to chapter 5

--- src/initial/plotting.py
+++ src/initial/plotting.py
@@ -1,7 +1,20 @@
 import matplotlib.pyplot as plt
+from collections import Counter
 
 def plot_words(word_counts, n=10):
-    """Plot a bar chart of word counts."""
+    """Plot a bar chart of word counts.
+
+    Parameters
+    ----------
+    word_counts : collections.Counter  <--------
+        Counter object of word counts.
+    n : int, optional
+        Plot the top n words. By default, 10.
+
+    ...rest of docstring hidden...
+    """
+    if not isinstance(word_counts, Counter):
+        raise TypeError("'word_counts' should be of type 'Counter'.")
     top_n_words = word_counts.most_common(n)
     word, count = zip(*top_n_words)
     fig = plt.bar(range(n), count)
@@ -9,4 +22,3 @@ def plot_words(word_counts, n=10):
     plt.xlabel("Word")
     plt.ylabel("Count")
     return fig
-
